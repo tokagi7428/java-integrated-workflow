c64cd7577d62709bead0ddf03bdd9bead06cbc94
tokagi7428 <firstman131@hotmail.com>
2024-02-10 16:43:07 +0700

    add file pre-commit.sh

--- git-hooks/pre-commit.sh
+++ git-hooks/pre-commit.sh
@@ -1,12 +1,1 @@
-task installGitHooks(type: Exec) {
-	description 'Installs the pre-commit.sh git hooks from /git-hooks.'
-	group 'git hooks'
-	workingDir rootDir
-	commandLine 'chmod'
-	args '-R', '+x', '.git/hooks/'
-	dependsOn copyGitHooks
-	onlyIf { isLinuxOrMacOs() }
-	doLast {
-		logger.info('Git hook installed successfully.')
-	}
-}
+./grawlew test
